a97cd98b50f2a1437522d09b5465986382ec7e1e
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-18 07:34:51 +0000

    Add project files.

---
+++ Palindrome Check 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Palindrome_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string"); //Asks the user to enter a string.
+            string userString = Console.ReadLine().ToLower(); //Inputs and converts the string to all lowercase so it isn't case sensitive.
+
+
+            for (int i = 0; i < userString.Length; i++) //Loops the following code for every character in the string.
+            {
+                if (userString[i] != userString[userString.Length - 1 - i]) //Checks if the cvharacters are not the same.
+                {
+                    Console.WriteLine($"{userString} is not a palindrome.");
+                    break; //Breaks the loop
+                }
+                else if (i == userString.Length - 1) //Checks if this is the last iteration of the loop
+                {
+                    Console.WriteLine($"{userString} is a palindrome.");
+                }
+            }
+        }
+    }
+}
